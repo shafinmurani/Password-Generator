35b895d2fda1b23f067759b6eea03c9ee973c652
Shafin Murani <66113323+shafinmurani@users.noreply.github.com>
2021-03-03 11:25:55 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,13 @@ import random
 import string
 
 try :
-    length = int(input('Input the length of the password : '))
+    length = int(input('Input the length of the password [Default : 20] : '))
+    if lenght >=10 : 
+        print("Please increase the length of the password, Your password is too short")
+    if length == '':
+        length = 20
+    elif length == ' ':
+        length == 20
 except NameError :
        print("Please Supply a valid integer")
 chars = str(string.ascii_letters + string.digits + string.punctuation)
