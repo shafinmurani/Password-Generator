9c94f4ba7caac7152952a13f696ff93caf8391a1
Shafin Murani <66113323+shafinmurani@users.noreply.github.com>
2021-03-03 17:38:43 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,21 @@
 #!/bin/python3
 import random
 import string
-
 try :
-    length = int(input('Input the length of the password : '))
+    length = input('Input the length of the password [default : 20] : ')
 except NameError :
        print("Please Supply a valid integer")
 except KeyboardInterrupt :
     print("\nUser interrupted the program \nExitting...")
-
+if length == '':
+    length = 20
+elif length == ' ' :
+    length = 20
 chars = str(string.ascii_letters + string.digits + string.punctuation)
-
+try : 
+    int(length)
+except ValueError:
+    print("Please Supply a valid integer")
 try :
     password = ''.join(random.choice(chars) for _ in range(length))
     print(password)
@@ -18,3 +23,5 @@ except NameError:
     print('')
 except KeyboardInterrupt :
     print("\nUser interrupted the program \nExitting...")
+except TypeError :
+    print('')
